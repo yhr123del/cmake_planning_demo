10b8eab2aa94e12e630af30962bcffb7f6708738
yhr123del <3186906327@qq.com>
2025-08-30 09:59:33 +0800

    base demo

---
+++ src/planning_main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "process.h"
+using namespace std;
+
+int main()
+{
+    cout << "planing start" << endl;
+    Process pro;
+    pro.planProcess();
+    cout << "planing end" << endl;
+
+    return 0;
+}
